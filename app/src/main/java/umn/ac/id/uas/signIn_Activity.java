4faf5d1d00a13055d890f3a41ade4a0afff36122
atras30 <atrasshalhan@gmail.com>
2022-11-14 00:12:14 +0700

    Login by google done

--- app/src/main/java/umn/ac/id/uas/signIn_Activity.java
+++ app/src/main/java/umn/ac/id/uas/signIn_Activity.java
@@ -6,7 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
@@ -18,10 +27,17 @@ import umn.ac.id.uas.retrofit.UserModel;
 
 public class signIn_Activity extends AppCompatActivity {
     TextView signUp, btnReady, title;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    GoogleSignInAccount acct = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
 
         APIService.endpoint().getUser().enqueue(new Callback<UserModel>() {
             @Override
@@ -61,5 +77,34 @@ public class signIn_Activity extends AppCompatActivity {
                 startActivity(MoveToMainActivity);
             }
         });
+
+        TextView loginWithGoogleButton = findViewById(R.id.Google);
+
+        loginWithGoogleButton.setOnClickListener(v -> {
+            loginWithGoogle();
+        });
+    }
+
+    private void loginWithGoogle() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                acct = GoogleSignIn.getLastSignedInAccount(this);
+
+                Toast.makeText(this, "Welcome, " + acct.getDisplayName(), Toast.LENGTH_SHORT).show();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong...", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
